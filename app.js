8764cd7ca582ebe4a6597385c647bd314c5abee9
sigbuz <sigitirawan22ixc@gmail.com>
2023-01-05 13:48:03 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'list'
+});
+
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
+
+app.get('/index', (req, res) => {
+  connection.query(
+    'SELECT * FROM items',
+    (error, results) => {
+      res.render('index.ejs', {items: results});
+    }
+  );
+});
+
+app.get('/new', (req, res) => {
+  res.render('new.ejs');
+});
+
+app.post('/create', (req, res) => {
+  connection.query(
+    'INSERT INTO items (name) VALUES (?)',
+    [req.body.itemName],
+    (error, results) => {
+      res.redirect('/index');
+    }
+  );
+});
+
+// Tambahkan route untuk menghapus item
+
+
+app.listen(3000);
